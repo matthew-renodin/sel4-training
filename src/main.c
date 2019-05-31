691658397e95f9d340f24f221204a2ccdd9cd30c
Your Name <you@example.com>
2019-05-31 16:04:27 -0400

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(int argc, char *argv[]) {
+
+    while(1){
+      for(int i = 0; i< 20;i++){
+		printf("   \n");
+		ps_udelay(100*130);
+      }
+
+
+printf("             _   _  _                           \n");ps_udelay(100*130);
+printf("            | | | || |                          \n");ps_udelay(100*130);
+printf("    ___  ___| | | || |_                         \n");ps_udelay(100*130);
+printf("   / __|/ _ \\ | |__   _|                        \n");ps_udelay(100*130);
+printf("   \\__ \\  __/ |____| |                          \n");ps_udelay(100*130);
+printf("  _|___/\\___|______|_|   _                      \n");ps_udelay(100*130);
+printf(" |__   __|      (_)     (_)                     \n");ps_udelay(100*130);
+printf("    | |_ __ __ _ _ _ __  _ _ __   __ _          \n");ps_udelay(100*130);
+printf("    | | '__/ _` | | '_ \\| | '_ \\ / _` |         \n");ps_udelay(100*130);
+printf("    | | | | (_| | | | | | | | | | (_| |         \n");ps_udelay(100*130);
+printf("    |_|_|  \\__,_|_|_| |_|_|_| |_|\\__, |         \n");ps_udelay(100*130);
+printf("              ___   ___  __  ___  __/ |         \n");ps_udelay(100*130);
+printf("             |__ \\ / _ \\/_ |/ _ \\|___/          \n");ps_udelay(100*130);
+printf("  ______ ______ ) | | | || | (_) |______ ______ \n");ps_udelay(100*130);
+printf(" |______|______/ /| | | || |\\__, |______|______|\n");ps_udelay(100*130);
+printf("              / /_| |_| || |  / /               \n");ps_udelay(100*130);
+printf("             |____|\\___/ |_| /_/                \n");ps_udelay(100*130);
+
+        
+
+
+    }
+}
+
